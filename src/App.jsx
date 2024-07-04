5dab13f0a9a55a60cf1cfda7f21fa751153673be
Trần Công Mạnh <120402571+Congmanh270504@users.noreply.github.com>
2024-07-04 18:46:45 +0700

    #1- delete to update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 import './App.css';
-import MainContent from './main-content.jsx';
+import './index.css';
+import MainContent from './right-content/main-content.jsx';
 import Navbar from './left-content/Navbar.jsx';
-import Search from './left-content/search.jsx';
-import Logo from './left-content/logo.jsx';
-import logoImage from './logo.jpg';
-import HideShow from './left-content/hide.jsx';
 function App() {
   return (
     <>
-      <div className='content'>
-        <div className='left-content'>
-          <HideShow />
-          <Search />
+      <div className='container'>
+        <div className='left-container'>
           <Navbar />
-          <Logo src={logoImage} />
-          <p>Create by Thomas 6/2024</p>
         </div>
-        <MainContent />
+        <div className='right-container'>
+          <MainContent />
+        </div>
       </div>
     </>
   );
---
+++ src/right-content/main-content.jsx
@@ -0,0 +1,13 @@
+import "../App.css";
+function MainContent() {
+    return (
+        <div className="main-content section-p1">
+            <h1 className="text-3xl font-bold underline font-light">
+                Hello world!
+            </h1>
+            <p>Test</p>
+        </div>
+    );
+}
+
+export default MainContent;
